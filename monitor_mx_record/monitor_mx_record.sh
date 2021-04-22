c9263dad08967b31cbbbc8f551762cb4861af843
Andre Boyce <mobile@andreboyce.com>
2021-04-22 08:24:33 -0400

    update

--- monitor_mx_record/monitor_mx_record.sh
+++ monitor_mx_record/monitor_mx_record.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Monitor mx record
-# This script will check the mx record of a domain name against a predefined value for a given list of domains.
+# Monitor MX Record
+# This script will check the MX record of a domain name against a predefined value for a given list of domains.
 # It will sort the mx records for the domain numerically by priority and use the lowest value.
+
+# Usage
 # Make the file executable and copy it into /etc/cron.hourly
-# It assumes that dig is installed
+# chmod +x monitor_mx_records.sh; mv monitor_mx_records.sh /etc/cron.hourly/monitor_mx_records.sh;
+# It assumes that dig is installed (yum install bind-utils, apt install dig -y)
 
 # To check the expected record for a domain e.g. google.com
-# dig google.com MX +short | sort -u | head -n1 | awk '{print $NF}' | tr '[:upper:]' '[:lower:]'
+# dig google.com MX +short | sort -n | head -n1 | awk '{print $NF}' | tr '[:upper:]' '[:lower:]'
 
 # live will send an email
 LIVE=1; # yes=1, no=0;
@@ -36,14 +39,14 @@ MX_RECORDS['google.com']='aspmx.l.google.com.';
 for DOMAIN in "${!MX_RECORDS[@]}";
 do
 
-   MX_RECORD=$(dig $DOMAIN MX +short | sort -u | head -n1 | awk '{print $NF}' | tr '[:upper:]' '[:lower:]');
+   MX_RECORD=$(dig $DOMAIN MX +short | sort -n | head -n1 | awk '{print $NF}' | tr '[:upper:]' '[:lower:]');
    EXPECTED_MX_RECORD=${MX_RECORDS[$DOMAIN]};
 
-   # test {} = $EXPECTED_MX_RECORD && "yes match" || "no match"
+   # test {MX_RECORD} = $EXPECTED_MX_RECORD && "yes match" || "no match"
    if [ $LIVE -eq 0 ]; then
-     echo $MX_RECORD | xargs -I{} test {} = $EXPECTED_MX_RECORD && : || echo "Subject: MX record for $DOMAIN, $MX_RECORD does not match $EXPECTED_MX_RECORD";
+     echo $MX_RECORD | xargs -I{MX_RECORD} test {MX_RECORD} = $EXPECTED_MX_RECORD && : || echo "Subject: MX record for $DOMAIN, $MX_RECORD does not match $EXPECTED_MX_RECORD";
    elif [ $LIVE -eq 1 ]; then
-     echo $MX_RECORD | xargs -I{} test {} = $EXPECTED_MX_RECORD && : || echo "Subject: MX record for $DOMAIN, $MX_RECORD does not match $EXPECTED_MX_RECORD" | tee /dev/stderr | sendmail $EMAIL;
+     echo $MX_RECORD | xargs -I{MX_RECORD} test {MX_RECORD} = $EXPECTED_MX_RECORD && : || echo "Subject: MX record for $DOMAIN, $MX_RECORD does not match $EXPECTED_MX_RECORD" | tee /dev/stderr | sendmail $EMAIL;
    else
      : # do nothing
    fi
